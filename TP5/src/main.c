9c768c0bb8cfe7abb573432c9887a021d9e79193
Rossamylne <l.bader26@yahoo.com>
2024-09-30 10:08:36 +0200

    Finished algo for TP5

--- TP5/src/main.c
+++ TP5/src/main.c
@@ -12,25 +12,51 @@
 int main(int argc, char **argv)
 {
     printf("\nBase de projet BUT GEII - Module Info1\n\n");
-        /* environnement */ /* rôle des objets */
-        /* environnement */ /* rôle des objets */
     int NB_MAX = 40;
-    const CAR_FIN = "\r";
-    int i, nbCar;
-    char word[NB_MAX];
-    char car;
-    /* Algorithm*//* condition */
-    printf("\nType chain of caracter : ");
+    char CAR_FIN = '\n';
+    int i, nbCar = 0;
+    char word[NB_MAX], reverseWord[NB_MAX], car;
+    bool areCharSame = true;
+
+    /* Algorithm */
+    printf("\nType chain of characters (press Enter to finish): ");
     do {
         car = getchar();
-        if((car != CAR_FIN) && (car != " ") && (nbCar < NB_MAX)) {
+        if ((car != CAR_FIN) && (car != ' ') && (nbCar < NB_MAX)) {
             word[nbCar] = car;
             nbCar = nbCar + 1;
-            printf("\n table --> %c\n", word);
         }
-    } while((car != CAR_FIN) || (nbCar == 0));
-    for(i = 0;i <= nbCar-1;i++) {
-        printf("%d", i);
+    } while (car != CAR_FIN);
+    for (i = 0; i < nbCar; i++) {
+        reverseWord[i] = word[nbCar - 1 - i];
     }
+    printf("\nCharacters in reverse order: ");
+    for(i = 0; i < nbCar; i++) {
+        printf("%c", reverseWord[i]);
+    }
+    printf("\nIndexes of characters: ");
+    for (i = 0; i < nbCar; i++) {
+        printf("%d ", i);
+    }
+    for (i = 0; i < nbCar; i++) {
+        if (word[i] != reverseWord[i]) {
+            areCharSame = false;
+            break;
+        }
+    }
+    switch (areCharSame) {
+        case true:
+            printf("\nThe word \"%s\" is a palindrome\n", word);
+            break;
+
+        case false:
+            printf("\nThe word \"%s\" is not a palindrome\n", word);
+            break;
+
+        default:
+            printf("\nError\n");
+            break;
+    }
+
     return 0;
 }
