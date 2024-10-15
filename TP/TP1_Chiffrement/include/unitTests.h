7de53878179570b8b6c106ab1e1dfc0c3d32525f
Rossamylne <l.bader26@yahoo.com>
2024-10-15 18:08:19 +0200

    Deleted some useless stuff

--- TP/TP1_Chiffrement/include/unitTests.h
+++
@@ -1,8 +0,0 @@
-#ifndef UNITTESTS_H_
-#define UNITTESTS_H_
-
-void testAcquerirCle();
-void testChiffrerMin();
-void testChiffrerMaj();
-
-#endif //UNITTESTS_H_
--- TP/TP1_Chiffrement/src/readme.txt
+++
@@ -1,3 +0,0 @@
-Il est nécessaire, dans VSCode, d'installer les plugins suivants:
-C/C++
-Makefile Tools
