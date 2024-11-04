7965de183babf5640bc5474759d252985ef2171a
Rossamylne <l.bader26@yahoo.com>
2024-11-04 11:51:28 +0100

    Updated TP3

--- TP/TP3_IHM/src/actionsParametrees.c
+++ TP/TP3_IHM/src/actionsParametrees.c
@@ -6,17 +6,38 @@
 
 void afficheTrain(int x, int y, unsigned long couleur)
 {
-
+    dessineRectanglePlein(x+20, y-47, 54, 10, IHM_Window_couleur(0,0,0));
+    dessineRectanglePlein(x+60, y-47, 54, 10, IHM_Window_couleur(0,0,0));
+    dessineRectanglePlein(x, y-LARGEUR_TRAIN, LARGEUR_TRAIN, LARGEUR_TRAIN*2, couleur);
+    for(int i=0; i<20; i++){
+        dessineLigne(x+80, y-LARGEUR_TRAIN,  x+80+i, y-i, couleur);
+    }
 }
 
 void afficheRails(int x, int y){
-
+    int w,h;
+    int i;
+    getWindowSize(&w, &h);
+    dessineLigne(0, y+2, w, y+2, IHM_Window_couleur(0,0,255));
+    dessineLigne(0, y-42, w, y-42, IHM_Window_couleur(0,0,255));
+    for(i=0;i<w;i++) {
+        dessineRectanglePlein(i*20, y-42, 44, 3, IHM_Window_couleur(0,0,255));
+    }
 }
 
 void affichePalette(unsigned long palette[], int nbCouleurs){
-
+    int w, h;
+    getWindowSize(&w, &h);
+    for(int i=0;i<nbCouleurs;i++) {
+        dessineRectanglePlein(i*(w/5),h-10, (i+1)*((w/5)-1),h, palette[i]);
+    }
 }
 
 int getIndiceCouleur(int x, int nbCouleurs){
-    return -1;
+    int coul, w, h;
+    getWindowSize(&w, &h);
+    if(h-10){
+        coul = x % (w/nbCouleurs);
+    }
+    return coul;
 }
--- TP/TP3_IHM/src/main.c
+++ TP/TP3_IHM/src/main.c
@@ -11,8 +11,7 @@
 #include "IHM_Setup.h"
 #include "actionsParametrees.h"
 #include "main.h"
-
-
+#define NB_COUL = 5;
 int main(int argc, char **argv)
 {
     //////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,32 +25,45 @@ int main(int argc, char **argv)
 
     // Mon environnement ici ////////////////////////////
 
+    int xTrain, Ytrain;             /* Position du train */
+    int xClic, yClic, btnClick;     /* Position du dernier clic */
+    long unsigned int mapalette[5];               /* Palette de couleur */
+    char keyPressed;                /* Dernier caractère tapé */
+    int railsVisibles = 0;          /* Si vrai afficher les rails */
+    int trainVisible = 0;           /* Si vrai afficher le train */
     int xSouris = 0;
     int ySouris = 0;
     int carreVisible = 0;
-    char keyPressed = 0;
 
     do
     {
-
+        mapalette[0] = IHM_Window_couleur(142, 202, 230);
+        mapalette[1] = IHM_Window_couleur(33, 158, 188);
+        mapalette[2] = IHM_Window_couleur(2, 48, 71);
+        mapalette[3] = IHM_Window_couleur(255, 183, 3);
+        mapalette[4] = IHM_Window_couleur(251, 133, 0);
         IHM_Application_clearWindows(&application);
         IHM_Application_processEvent(&application);
 
         //Mon Code Ici /////////////////////////////////////////////
-
+        getMouseClick(&xClic, &yClic);
         keyPressed = getKeyPressed();
-        if (keyPressed == 'c') carreVisible = !carreVisible;
-
+        if(keyPressed == 'r')  railsVisibles = !railsVisibles;
+        if(keyPressed == 't')  trainVisible = !trainVisible;
         getMousePosition(&xSouris, &ySouris);
-        if (carreVisible == 1) dessineRectanglePlein(xSouris,ySouris,40,40, IHM_Window_couleur(255,0,0));
-
+        if(carreVisible == 1) dessineRectanglePlein(xSouris,ySouris,40,40, IHM_Window_couleur(255,0,0));
+        if(railsVisibles) afficheRails(xSouris,ySouris);
+        if(trainVisible) afficheTrain(xSouris,ySouris, mapalette[getIndiceCouleur(xClic, 5)]);
+        printf("%d", mapalette[getIndiceCouleur(xClic, 5)]);
+        affichePalette(mapalette, 5);
         //Fin de mon code ////////////////////////////////////////
 
         IHM_Application_drawWindows(&application);
  
 
-    } while (!application.quitter); //On quitte soit par la touche ESC, soit par la fermeture de la fenêtre
+    } while (!application.quitter); //On quitte soif(railsVisibles == true) it par la touche ESC, soit par la fermeture de la fenêtre
     // fin de la boucle principale
     IHM_Application_destroy(&application);
     return 0;
 }
+ 
