48159be35f34014b6f85b1c26a4b1f0643ec2250
Rossamylne <l.bader26@yahoo.com>
2024-12-13 13:32:36 +0100

    Updated graphics

--- PT/GameOfLife_Project/src/fonctionsGraphiques.c
+++ PT/GameOfLife_Project/src/fonctionsGraphiques.c
@@ -11,6 +11,7 @@ void dessineGrille(void)
     dessineRectanglePlein(5, 60, h-90, w-10, IHM_Window_couleur(255,255,255));
 
     int case_width = (w-10) / 80;
+    dessineLigne(w-5, 60, w-5, h-90, IHM_Window_couleur(0, 0, 0)); 
     
     for (int i = 0; i <= w-5; i+=case_width) {
         int x = i + 5; 
@@ -18,6 +19,7 @@ void dessineGrille(void)
     }
 
     int case_height = (h - 90) / 80;
+    dessineLigne(5, h-90, w-5, h-30, IHM_Window_couleur(0, 0, 0));
 
     for (int j = 0; j <= h-90; j+=case_height) {
         int y = 60 + j;
